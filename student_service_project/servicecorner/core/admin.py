4f734dc6eeaec8ac108007f19aea9b6a112ca720
jaineesh <jaineeshshah2812@gmail.com>
2026-01-02 20:59:03 +0530

    registered models in admin

--- student_service_project/servicecorner/core/admin.py
+++ student_service_project/servicecorner/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+#from django.contrib import admin
+from .models import TokenSection, TokenBooking, ChatMessage, Feedback, UnansweredQuestion
+
+# Saare models ko register kar do taaki Admin mein dikhne lagein
+admin.site.register(TokenSection)
+admin.site.register(TokenBooking)
+admin.site.register(ChatMessage)
+admin.site.register(Feedback)
+admin.site.register(UnansweredQuestion)
