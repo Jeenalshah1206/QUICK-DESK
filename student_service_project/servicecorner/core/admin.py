fab65f0d0fdd18fefd32ea19ec0a0246b7395b16
jaineesh <jaineeshshah2812@gmail.com>
2026-01-02 21:33:34 +0530

    Final fix: SITE_ID and Admin models

--- student_service_project/servicecorner/core/admin.py
+++ student_service_project/servicecorner/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 #from django.contrib import admin
-from .models import TokenSection, TokenBooking, ChatMessage, Feedback, UnansweredQuestion
+from .models import Feedback,TokenSection, TokenBooking, ChatMessage, Feedback, UnansweredQuestion
 
 # Saare models ko register kar do taaki Admin mein dikhne lagein
 admin.site.register(TokenSection)
@@ -10,3 +10,5 @@ admin.site.register(TokenBooking)
 admin.site.register(ChatMessage)
 admin.site.register(Feedback)
 admin.site.register(UnansweredQuestion)
+
+admin.site.register(Feedback)
